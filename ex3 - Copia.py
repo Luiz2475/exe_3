3dc65e331b977965f497505857cf1ff176737989
Luiz2475 <luizguihermmelgaz@gmail.com>
2024-05-17 21:26:13 -0300

    segundo commit

--- ex3 - Copia.py
+++ ex3 - Copia.py
@@ -1,26 +1,72 @@
 print(' 1-Adicionar produtos \n 2-Atualizar a quantidade \n 3-Consultar quantidade')
 opcao = int(input('Insira a opção desejada: '))
-while opcao > 3: 
+while opcao not in [1, 2, 3]: 
    opcao = int(input('Insira a opção desejada: '))
 estoque = {}
 if opcao == 1:
    produto = str(input('Digite o nome do produto: '))
    estoque[produto] = int(input('Digite a quantidade: '))
-   print(estoque)
+   print('Produto adicionado')
    print(' 1-Adicionar produtos \n 2-Atualizar a quantidade \n 3-Consultar quantidade')
-   opcao = 0
    opcao = int(input('Insira a opção desejada: '))
    while opcao > 3: 
-    opcao = input('Insira a opção desejada: ')
-    if opcao == 1:
+     opcao = int(input('Insira a opção desejada: '))
+   if opcao == 1:
        produto = str(input('Digite o nome do produto: '))
        estoque[produto] = int(input('Digite a quantidade: '))
-    elif opcao == 2:
-       print("a")
-       atualizarProduto = int(input('Insira o nome do Produto: '))
-       if atualizarProduto == produto:
-          estoque[produto] = int(input('Insira nova quantidade: '))  
-          print(estoque)
+       print(' 1-Adicionar produtos \n 2-Atualizar a quantidade \n 3-Consultar quantidade')
+   opcao = int(input('Insira a opção desejada: '))
+   while opcao > 3: 
+     opcao = int(input('Insira a opção desejada: '))
+   if opcao == 1:
+       produto = str(input('Digite o nome do produto: '))
+       estoque[produto] = int(input('Digite a quantidade: '))
+   elif opcao == 2:
+      atualizarProduto = input('Insira o nome do Produto: ')
+      if atualizarProduto == produto:
+         estoque[produto] = int(input('Insira nova quantidade: '))  
+         print(f'A nova quantidade do produto é : {estoque[produto]}')
+   elif opcao == 3: 
+      saber = input("Produto desejado: ")
+      print(estoque[saber])
+   elif opcao == 2:
+      atualizarProduto = input('Insira o nome do Produto: ')
+      if atualizarProduto == produto:
+         estoque[produto] = int(input('Insira nova quantidade: '))  
+         print(f'A nova quantidade do produto é : {estoque[produto]}')
+         print(' 1-Adicionar produtos \n 2-Atualizar a quantidade \n 3-Consultar quantidade')
+   opcao = int(input('Insira a opção desejada: '))
+   while opcao > 3: 
+     opcao = int(input('Insira a opção desejada: '))
+   if opcao == 1:
+       produto = str(input('Digite o nome do produto: '))
+       estoque[produto] = int(input('Digite a quantidade: '))
+   elif opcao == 2:
+      atualizarProduto = input('Insira o nome do Produto: ')
+      if atualizarProduto == produto:
+         estoque[produto] = int(input('Insira nova quantidade: '))  
+         print(f'A nova quantidade do produto é : {estoque[produto]}')
+   elif opcao == 3: 
+      saber = input("Produto desejado: ")
+      print(estoque[saber])
+   elif opcao == 3: 
+      saber = input("Produto desejado: ")
+      print(estoque[saber])
+      print(' 1-Adicionar produtos \n 2-Atualizar a quantidade \n 3-Consultar quantidade')
+   opcao = int(input('Insira a opção desejada: '))
+   while opcao > 3: 
+     opcao = int(input('Insira a opção desejada: '))
+   if opcao == 1:
+       produto = str(input('Digite o nome do produto: '))
+       estoque[produto] = int(input('Digite a quantidade: '))
+   elif opcao == 2:
+      atualizarProduto = input('Insira o nome do Produto: ')
+      if atualizarProduto == produto:
+         estoque[produto] = int(input('Insira nova quantidade: '))  
+         print(f'A nova quantidade do produto é : {estoque[produto]}')
+   elif opcao == 3: 
+      saber = input("Produto desejado: ")
+      print(estoque[saber])
 elif opcao == 2:
    print("Estoque vazio")
 elif opcao == 3:
